13648e4b9f188270e7329d9db1a559ba2391fd5b
kara <89068705+gongitsune@users.noreply.github.com>
2024-04-28 23:22:52 +0900

    プレイヤーの操作を実装

---
+++ Assets/Projects/Scripts/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Projects.Scripts.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float speed, rotatePower, verticalPower;
+
+        private PlayerInput _input;
+        private Rigidbody _rigid;
+        private Vector2 _rotation;
+
+        private void Start()
+        {
+            _input = new PlayerInput();
+            TryGetComponent(out _rigid);
+        }
+
+        private void Update()
+        {
+            _input.CheckInput();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_input.LeftWing && _input.RightWing)
+                _rotation.x -= verticalPower * Time.fixedDeltaTime;
+            else if (!_input.LeftWing && !_input.RightWing)
+                _rotation.x += verticalPower * Time.fixedDeltaTime;
+
+            if (_input.LeftWing)
+                _rotation.y -= rotatePower * Time.fixedDeltaTime;
+            if (_input.RightWing)
+                _rotation.y += rotatePower * Time.fixedDeltaTime;
+            _rigid.MoveRotation(Quaternion.Euler(_rotation.x, _rotation.y, 0));
+            _rigid.MovePosition(_rigid.position + transform.forward * (speed * Time.fixedDeltaTime));
+        }
+    }
+}
---
+++ Assets/Projects/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Projects.Scripts.Player
+{
+    public class PlayerInput
+    {
+        public bool RightWing { get; private set; }
+        public bool LeftWing { get; private set; } 
+        
+        public void CheckInput()
+        {
+            LeftWing = Input.GetKey(KeyCode.Z);
+            RightWing = Input.GetKey(KeyCode.M);
+        }
+    }
+}
