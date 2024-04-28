e9fa75386470ca2db14dd1e1ff4174aed2e3ca41
kara <89068705+gongitsune@users.noreply.github.com>
2024-04-29 03:00:45 +0900

    ステージに木を追加

---
+++ Assets/Projects/Scripts/Structures/Level.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Projects.Scripts.Structures
+{
+    public class Level : MonoBehaviour
+    {
+        public Collider[] Collider { get; private set; }
+
+        private void Start()
+        {
+            Collider = GetComponentsInChildren<Collider>();
+        }
+
+        public void SetColliderActive(bool active)
+        {
+            foreach (var col in Collider) col.enabled = active;
+        }
+    }
+}
--- Assets/Projects/Scripts/Structures/LevelGenerator.cs
+++ Assets/Projects/Scripts/Structures/LevelGenerator.cs
@@ -9,26 +9,27 @@ namespace Projects.Scripts.Structures
 {
     public class LevelGenerator : MonoBehaviour
     {
-        [SerializeField] private GameObject[] levelPrefab;
+        [SerializeField] private Level[] levelPrefab;
         [SerializeField] private PlayerController player;
         [SerializeField] private float scanInterval = 0.5f;
         [SerializeField] private float maxRadius = 30f;
         [SerializeField] private float levelSize;
         [SerializeField] private Vector3 scale;
-        private readonly List<Transform> _generated = new();
+        private readonly List<Level> _generated = new();
         private float _lastScanTime, _lastGenerateTime;
 
-        private ObjectPool<GameObject> _pool;
+        private ObjectPool<Level> _pool;
 
         private void Start()
         {
-            _pool = new ObjectPool<GameObject>(() =>
-            {
-                var randomIndex = Random.Range(0, levelPrefab.Length);
-                var level = Instantiate(levelPrefab[randomIndex]);
-                level.transform.localScale = scale;
-                return level;
-            }, obj => obj.SetActive(true), obj => obj.SetActive(false));
+            _pool = new ObjectPool<Level>(() =>
+                {
+                    var randomIndex = Random.Range(0, levelPrefab.Length);
+                    var level = Instantiate(levelPrefab[randomIndex]);
+                    level.transform.localScale = scale;
+                    return level;
+                }, level => level.gameObject.SetActive(true),
+                level => level.gameObject.SetActive(false));
         }
 
         private void Update()
@@ -61,13 +62,12 @@ namespace Projects.Scripts.Structures
             {
                 var offsetPos = ((position + playerPos) / levelSize).Floor() * levelSize;
                 if ((offsetPos - playerPos).sqrMagnitude > maxRadius * maxRadius) continue;
-                if (_generated.Exists(structure => structure.position == offsetPos)) continue;
+                if (_generated.Exists(structure => structure.transform.position == offsetPos)) continue;
 
-                var randomIndex = Random.Range(0, levelPrefab.Length);
                 var level = _pool.Get();
                 level.transform.position = offsetPos;
                 level.transform.localScale = scale;
-                _generated.Add(level.transform);
+                _generated.Add(level);
             }
         }
 
@@ -79,11 +79,15 @@ namespace Projects.Scripts.Structures
             var playerPos = player.transform.position;
             _generated.ForEach(structure =>
             {
-                if ((structure.position - playerPos).sqrMagnitude > maxRadius * maxRadius)
-                    _pool.Release(structure.gameObject);
+                var structureDis = (structure.transform.position - playerPos).sqrMagnitude;
+                if (structureDis > maxRadius * maxRadius)
+                    _pool.Release(structure);
+                else if (structureDis < levelSize * levelSize)
+                    structure.SetColliderActive(true);
+                else structure.SetColliderActive(false);
             });
             _generated.RemoveAll(structure =>
-                (structure.position - playerPos).sqrMagnitude > maxRadius * maxRadius);
+                (structure.transform.position - playerPos).sqrMagnitude > maxRadius * maxRadius);
         }
     }
 }
